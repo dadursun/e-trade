798d55d68d350e2699ac1c572799062ab0c1b0a5
Davut Ahmet Dursun <dadursun@gmail.com>
2020-05-31 22:40:08 +0300

    Add project files.

---
+++ MvcWebUI/Helpers/CartSessionHelper.cs
@@ -0,0 +1,40 @@
+﻿using Entities.DomainModels;
+using Microsoft.AspNetCore.Http;
+using MvcWebUI.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcWebUI.Helpers
+{
+    public class CartSessionHelper : ICartSessionHelper
+    {
+        private IHttpContextAccessor _httpContextAccesor;
+
+        public CartSessionHelper(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccesor = httpContextAccessor;
+        }
+        public void Clear()
+        {
+            _httpContextAccesor.HttpContext.Session.Clear();
+        }
+
+        public Cart GetCart(string key)
+        {
+            Cart cartToCheck = _httpContextAccesor.HttpContext.Session.GetObject<Cart>(key);
+            if (cartToCheck==null)
+            {
+                SetCart(key, new Cart());
+                cartToCheck= _httpContextAccesor.HttpContext.Session.GetObject<Cart>(key);
+            }
+            return cartToCheck;
+        }
+
+        public void SetCart(string key,Cart cart)
+        {
+            _httpContextAccesor.HttpContext.Session.SetObject(key, cart);
+        }
+    }
+}
---
+++ MvcWebUI/ValidationRules/FluentValidation/ShippingDetailValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using MvcWebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcWebUI.ValidationRules.FluentValidation
+{
+    public class ShippingDetailValidator:AbstractValidator<ShippingDetail>
+    {
+        public ShippingDetailValidator()
+        {
+            RuleFor(s => s.FirstName).NotEmpty().WithMessage("Ad girilmelidir");
+            RuleFor(s => s.LastName).NotEmpty().WithMessage("Soyad girilmelidir");
+            RuleFor(s => s.Address).NotEmpty().WithMessage("Adres girilmelidir"); ;
+            RuleFor(s => s.City).NotEmpty().WithMessage("Şehir girilmelidir");
+            RuleFor(s => s.Email).NotEmpty().WithMessage("Email girilmelidir");
+
+
+
+
+        }
+
+       
+    }
+}
